ad0423f956c7abf36d26f514b670f907d0481354
Liangcheng Song <slc1@mail.ustc.edu.cn>
2024-11-27 18:44:50 +0800

    Update patcher.py
    不存在的generation_config对象

--- src/llamafactory/model/patcher.py
+++ src/llamafactory/model/patcher.py
@@ -137,13 +137,13 @@ def patch_model(
     is_trainable: bool,
     add_valuehead: bool,
 ) -> None:
-    gen_config = model.generation_config  # check and fix generation config
-    if not gen_config.do_sample and (
-        (gen_config.temperature is not None and gen_config.temperature != 1.0)
-        or (gen_config.top_p is not None and gen_config.top_p != 1.0)
-        or (gen_config.typical_p is not None and gen_config.typical_p != 1.0)
-    ):
-        gen_config.do_sample = True
+    # gen_config = model.generation_config  # check and fix generation config
+    # if not gen_config.do_sample and (
+    #     (gen_config.temperature is not None and gen_config.temperature != 1.0)
+    #     or (gen_config.top_p is not None and gen_config.top_p != 1.0)
+    #     or (gen_config.typical_p is not None and gen_config.typical_p != 1.0)
+    # ):
+    #     gen_config.do_sample = True
 
     if "GenerationMixin" not in str(model.generate.__func__):
         model.generate = MethodType(PreTrainedModel.generate, model)
