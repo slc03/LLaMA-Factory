1efaaff4a6f1a0fbd0f4400bb8e07c98f9813190
Liangcheng Song <slc1@mail.ustc.edu.cn>
2024-11-29 23:15:14 +0800

    Update template.py

--- src/llamafactory/data/template.py
+++ src/llamafactory/data/template.py
@@ -1249,9 +1249,10 @@ _register_template(
 _register_template(
     name="my_critic",
     format_user=StringFormatter(slots=["<|im_start|>user\n{{content}}<|im_end|>\n<|im_start|>assistant\n"]),
+    format_assistant=StringFormatter(slots=["{{content}}<|im_end|>\n[UNUSED_TOKEN_130]"]), # not predict
     format_system=StringFormatter(slots=["<|im_start|>system\n{{content}}<|im_end|>\n"]),
-    format_separator=EmptyFormatter(slots=["<|im_end|>\n[UNUSED_TOKEN_130]"]),
+    format_separator=EmptyFormatter(slots=["<|im_end|>\n"]),    # not used
     format_prefix=EmptyFormatter(slots=[{"bos_token"}]),
     stop_words=["<|im_end|>"],
-    efficient_eos=True,  # internlm2 tokenizer cannot set eos_token_id
+    # efficient_eos=True,  # internlm2 tokenizer cannot set eos_token_id
 )
